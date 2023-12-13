82144e914ad0e0436d8bad3a9e75f2ff6c7e13df
decadezzz <2979832814@qq.com>
2023-12-13 11:47:00 +0800

    课程管理

--- springboot/src/main/java/com/exam/MybatisPlusConfig.java
+++ springboot/src/main/java/com/exam/MybatisPlusConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+
+
 @EnableTransactionManagement
 @Configuration
 @MapperScan("com.exam.service.*.mapper*")
@@ -17,5 +22,10 @@ public class MybatisPlusConfig {
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
+    }
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
     }
 }
